b7199783138aa6970b0c43c24731068a6f8a46b4
Anthony <anthonysiu@gmail.com>
2015-08-02 22:35:42 -0500

    problem set 2 polygon

--- js/drawing.js
+++ js/drawing.js
@@ -9,33 +9,29 @@ var Drawing = (function(global) {
         win = global.window,
         canvas = doc.createElement('canvas');
 		
-	canvas.width = canvas_width;
-    canvas.height = canvas_height;
-	//doc.body.appendChild(canvas);
-	
-	var canvasRatio = canvas.width / canvas.height;
-	
+	var canvasWidth = 846;
+	var canvasHeight = 494;
+	var canvasRatio = canvasWidth / canvasHeight;
 	// scene
 	scene = new THREE.Scene();
 
 	// Camera: Y up, X right, Z up
-	windowScale = 10;
+	windowScale = 4;
 	var windowWidth = windowScale * canvasRatio;
-	var windowHeight = windowScale;	
+	var windowHeight = windowScale;
+
+	camera = new THREE.OrthographicCamera( windowWidth / - 2, windowWidth / 2, windowHeight / 2, windowHeight / - 2, 0, 40 );
 	
-	camera = new THREE.OrthographicCamera( windowWidth / - 2, windowWidth / 2,
-		windowHeight / 2, windowHeight / - 2, 0, 40 );
-	
-	var focus = new THREE.Vector3( 5,4,0 );
+	var focus = new THREE.Vector3( 0,1,0 );
 	camera.position.x = focus.x;
 	camera.position.y = focus.y;
 	camera.position.z = 10;
-	camera.lookAt( focus );
+	camera.lookAt(focus);
 
-	renderer = new THREE.WebGLRenderer({ antialias: true, preserveDrawingBuffer: true});
+	renderer = new THREE.WebGLRenderer({ antialias: false, preserveDrawingBuffer: true});
 	renderer.gammaInput = true;
 	renderer.gammaOutput = true;
-	renderer.setSize( canvas.width, canvas.height );
+	renderer.setSize( canvasWidth, canvasHeight );
 	//renderer.setClearColorHex( 0xffffff, 1.0 );
 	
 	document.body.appendChild( renderer.domElement );
@@ -43,11 +39,40 @@ var Drawing = (function(global) {
 	window.onload = function(){
 		init();
 	}
+	function PolygonGeometry(sides) {
+		var geo = new THREE.Geometry();
+		
+		// generate vertices
+		for ( var pt = 0 ; pt < sides; pt++ )
+		{
+			// Add 90 degrees so we start at +Y axis, rotate counterclockwise around
+			var angle = (Math.PI/2) + (pt / sides) * 2 * Math.PI;
+
+			var x = Math.cos( angle );
+			var y = Math.sin( angle );
+			
+			// YOUR CODE HERE
+			//Save the vertex location - fill in the code
+			geo.vertices.push( new THREE.Vector3( x, y, 0 ) );
+		}
+		// YOUR CODE HERE
+		// Write the code to generate minimum number of faces for the polygon.
+		/*geo.faces.push( new THREE.Face3( 0, 1, 2 ) );
+		geo.faces.push( new THREE.Face3( 2, 3, 0 ) );
+		geo.faces.push( new THREE.Face3( 3, 4, 0 ) );*/
+		//geo.faces.push( new THREE.Face3( 1, 2, 4 ) );
+		for (var face = 0; face < sides-2; face++){
+			geo.faces.push(new THREE.Face3(0,face+1, face+2));
+		}
+		// Return the geometry object
+		return geo;
+	}
+	
 	function init() {
-		var geometry = new THREE.BoxGeometry( 5, 5, 5 );
-            var material = new THREE.MeshLambertMaterial( { color: 0xFF0000 } );
-            var mesh = new THREE.Mesh( geometry, material );
-            scene.add( mesh );
+		var geo = PolygonGeometry(5);
+		var material = new THREE.MeshBasicMaterial( { color: 0xff0000, side: THREE.FrontSide } );
+		var mesh = new THREE.Mesh( geo, material );
+		scene.add( mesh );
 			
 			var light = new THREE.PointLight( 0xFFFF00 );
             light.position.set( 10, 0, 10 );
